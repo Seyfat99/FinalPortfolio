0615e04b0e43203851e3d57eb798e2a8704d32bc
Seyfat Khamidov <skhamidov@rv-clt-skhamidov.redventures.net>
2018-10-06 10:07:57 -0400

    Updates

---
+++ precache-manifest.0e7dd14ea621e9ca9c2523e12362cc0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a8629ac737458a495d1",
+    "url": "/FinalPortfolio/static/js/runtime~main.5a8629ac.js"
+  },
+  {
+    "revision": "915af58c6004cf7dee43",
+    "url": "/FinalPortfolio/static/js/main.915af58c.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/FinalPortfolio/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "915af58c6004cf7dee43",
+    "url": "/FinalPortfolio/static/css/main.482662b3.chunk.css"
+  },
+  {
+    "revision": "548fb098781f80df4590e624111a3237",
+    "url": "/FinalPortfolio/index.html"
+  }
+];
