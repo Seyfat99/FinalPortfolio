4d3a0ccc7bd0c7539acbdd3ab012f9f19b4e0751
Seyfat Khamidov <skhamidov@rv-clt-skhamidov.redventures.net>
2018-10-06 15:15:02 -0400

    deleted all images and use links from websites

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,12 @@
 import React from "react";
+import DownArrow from '../../assets/img/down-arrow.svg'
 
 const Hero = () => {
   return (
     <div className="hero">
       <p>I'm A Junior Web Developer</p>
       <h1>Seyfat Khamidov</h1>
-      <img src="../../../static/img/down-arrow.svg" alt="Down Arrow" />
+      <img src={DownArrow} alt="Down Arrow" />
     </div>
   );
 };
