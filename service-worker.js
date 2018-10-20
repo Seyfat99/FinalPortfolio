41b30217481dea3681b17db84796cfdae074c2c0
Seyfat Khamidov <skhamidov@rv-clt-skhamidov.redventures.net>
2018-10-20 00:24:09 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b9ed31b0c290b157f8c143a95b88dee.js"
+  "/FinalPortfolio/precache-manifest.03c6f3ac1b6ec1c859ecac06f3f95542.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/FinalPortfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
